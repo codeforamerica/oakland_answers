ccc54060e481313c99536945db8ed8820aaac5d1
Eddie A Tejeda <eddie@codeforamerica.org>
2013-05-30 16:06:16 -0700

    configure send mail

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "noreply@honolulu.gov"
+  config.mailer_sender = ENV['ACTION_MAILER_URL']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
