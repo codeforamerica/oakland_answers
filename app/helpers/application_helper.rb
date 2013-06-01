1dd09c9b85a48bcf4da8c086aa98256e8857e8b0
Eddie A Tejeda <eddie@codeforamerica.org>
2013-06-01 13:15:52 -0700

    [WIP] remove hard coded links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,18 @@ module ApplicationHelper
 	def category_by_name(name)
 		Category.where(:name => name).first
 	end
+
+	def offical_government_short_url
+		ENV['OFFICIAL_GOVERMENT_URL']
+	end
+
+	def offical_government_long_url
+		ENV['OFFICIAL_GOVERMENT_URL']
+	end
+
+
+	def link_offical_government
+		link_to ( offical_government_short_url,  offical_government_long_url )
+	end
+
 end
