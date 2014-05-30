706ae5205fe1227707358dfa79fb0bcb1a8f7ab9
Alan deLevie <adelevie@gmail.com>
2014-05-30 15:19:10 -0400

    verbose spec errors for brakeman warnings

--- spec/security/brakeman_spec.rb
+++ spec/security/brakeman_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 require 'brakeman'
 
+def format_warnings(warnings, type)
+  warnings.join("\n") << "\n Run `rake 'security:#{type}[0]'` to debug."
+end
+
 describe "Answers" do
   let(:checks) do
     tracker = Brakeman.run({
@@ -12,18 +16,22 @@ describe "Answers" do
   end
   
   it "returns zero high confidence controller warnings" do
-    expect(checks.controller_warnings.length).to(eq(0), "Run `rake 'security:controllers[0]'` to debug.")
+    brakeman_warnings = checks.controller_warnings
+    expect(brakeman_warnings.length).to(eq(0), format_warnings(brakeman_warnings, "controllers"))
   end
   
   it "returns zero high confidence model warnings" do
-    expect(checks.model_warnings.length).to(eq(0), "Run `rake 'security:models[0]'` to debug.")
+    brakeman_warnings = checks.model_warnings
+    expect(brakeman_warnings.length).to(eq(0), format_warnings(brakeman_warnings, "models"))
   end
   
   it "returns zero high confidence template warnings" do
-    expect(checks.template_warnings.length).to(eq(0), "Run `rake 'security:template[0]'` to debug.")
+    brakeman_warnings = checks.template_warnings
+    expect(brakeman_warnings.length).to(eq(0), format_warnings(brakeman_warnings, "templates"))
   end
 
   it "returns zero high confidence other warnings" do
-    expect(checks.other_warnings.length).to(eq(0), "Run `rake 'security:other[0]'` to debug.")
+    brakeman_warnings = checks.warnings
+    expect(brakeman_warnings.length).to(eq(0), format_warnings(brakeman_warnings, "other"))
   end
 end
