7a343bd3fe6e60afbcbcba377d5f306d1ea5184c
Justin Grevich <justin@grevi.ch>
2014-06-06 16:21:29 -0700

    fixes 18f/answers#75 by making stop_words a symbol

--- lib/rails_nlp/text_analyser.rb
+++ lib/rails_nlp/text_analyser.rb
@@ -6,8 +6,6 @@ module RailsNlp
     # require 'big_huge_thesaurus'
     require 'facets/enumerable'
 
-    @@STOP_WORDS = ['a','able','about','across','after','all','almost','also','am','among','an','and','any','are','as','at','be','because','been','but','by','can','cannot','could','dear','did','do','does','either','else','ever','every','for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is','it','its','just','least','let','like','likely','may','me','might','most','must','my','neither','no','nor','not','of','off','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what','when','where','which','while','who','whom','why','will','with','would','yet','you','your']
-
     def collect_text( options )
       model = options[:model]
       text = ''
@@ -46,7 +44,7 @@ module RailsNlp
 
     def freq_map words
       words = words.split
-      words = words - @@STOP_WORDS
+      words = words - Article::STOP_WORDS
       
       # https://github.com/rubyworks/facets/blob/master/lib/core/facets/enumerable/frequency.rb
       return words.frequency
@@ -90,7 +88,7 @@ module RailsNlp
     #   words = text.split
 
     #   # remove stop words
-    #   words = words - @@STOP_WORDS
+    #   words = words - Article::STOP_WORDS
 
     #   # make the words a frequency hashmap
     #   words = words.frequency
