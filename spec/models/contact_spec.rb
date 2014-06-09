15d4e7002a910b5d684b84563819048f4fec114d
Amos Stone <amos@amosarts.com>
2014-06-09 11:05:12 -0700

    [cleanup] Converts indentation to spaces

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Contact do
-	it "is valid with a name" do
-		contact = Contact.new(
-				:name => 'Jeff Goldblum'
-			)
-		expect(contact).to be_valid
-	end
+  it "is valid with a name" do
+    contact = Contact.new(
+        :name => 'Jeff Goldblum'
+      )
+    expect(contact).to be_valid
+  end
 
-	it "is valid without a name" do
-		contact = Contact.new(
-				:department => 'Department of Better Technology'
-			)
-		expect(contact).to have(0).errors_on(:name)
-	end
+  it "is valid without a name" do
+    contact = Contact.new(
+        :department => 'Department of Better Technology'
+      )
+    expect(contact).to have(0).errors_on(:name)
+  end
 end
