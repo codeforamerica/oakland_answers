33689865f6333119cef1d46169af3e1df70afd4f
Erica Kwan <erica@codeforamerica.org>
2014-06-26 12:41:29 -0700

    Improving article index page and updating categories pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,24 +2,32 @@ Article.destroy_all
 Category.destroy_all
 
 Category.create([
-  { :name => "Formats" },
-  { :name => "Meows" },
+  { :name => "Formats", description: "Anything about formats" },
+  { :name => "Meows", description: "I like meows a lot" },
   { :name => "Camping"  },
   { :name => "Trailers" },
   { :name => "License Plates" },
-  { :name => "City Jobs" }
+  { :name => "City Jobs", description: "Get a Job!" }
 ])
 
 first_category_id = Category.first.id
 second_category_id = Category.second.id
 
 Article.create([
-  { :title => "Emphasize Words",                :content_main => "Some of these words *are emphasized*. Some of these words _are emphasized also_.",                 :preview => "emphasis",          :tags => "emph",   :status => "Published", :category_id => first_category_id },
-  { :title => "Cat News",                       :content_main => "I start my morning with a cup of coffee and [breaking cat news](http://www.breakingcatnews.com/)", :preview => "cat news",          :tags => "cats",   :status => "Published", :category_id => first_category_id },
-  { :title => "Quick Brown Fox",                :content_main => "The quick brown fox jumped over the lazy dog's back.",                                :preview => "lazy dogs",         :tags => "dogs",   :status => "Published", :category_id => first_category_id },
-  { :title => "Blockquotes, How do they work?", :content_main => "> This is a blockquote.",                                                                          :preview => "blockquote",        :tags => "blocks", :status => "Published", :category_id => first_category_id },
-  { :title => "Paragraphs",                     :content_main => "Paragraphs can be written like so. A paragraph is the basic block of Markdown.",                   :preview => "markdown",          :tags => "mark",   :status => "Published", :category_id => second_category_id },
-  { :title => "Make Inline Code",               :content_main => "You can also make `inline code` to add code into other things.",                                   :preview => "code",              :tags => "code",   :status => "Published", :category_id => second_category_id },
-  { :title => "Basic Formatting",               :content_main => "Basic formatting of *italics* and **bold** is supported. This *can be **nested** like* so.",       :preview => "nested",            :tags => "nest",   :status => "Published", :category_id => second_category_id },
-  { :title => "Oh My Gosh, Headers",            :content_main => "### Headings *can* also contain **formatting**",                                                   :preview => "header formatting", :tags => "format", :status => "Published", :category_id => second_category_id }
+  { :title => "How do I apply for a Business License?",
+    :content_main => "Some of these words *are emphasized*. Some of these words _are emphasized also_.",                 :preview => "emphasis",          :tags => "emph",   :status => "Published", :category_id => first_category_id },
+  { :title => "Where Can I Find Good Cat News?",
+    :content_main => "I start my morning with a cup of coffee and [breaking cat news](http://www.breakingcatnews.com/)", :preview => "cat news",          :tags => "cats",   :status => "Published", :category_id => first_category_id },
+  { :title => "What About the Quick Brown Fox? What Does He Do?",
+    :content_main => "The quick brown fox jumped over the lazy dog's back.",                                :preview => "lazy dogs",         :tags => "dogs",   :status => "Published", :category_id => first_category_id },
+  { :title => "Blockquotes, How do they work?",
+    :content_main => "> This is a blockquote.",                                                                          :preview => "blockquote",        :tags => "blocks", :status => "Published", :category_id => first_category_id },
+  { :title => "Can I Learn More About Paragraphs?",
+    :content_main => "Paragraphs can be written like so. A paragraph is the basic block of Markdown.",                   :preview => "markdown",          :tags => "mark",   :status => "Published", :category_id => second_category_id },
+  { :title => "How do I Make Inline Code?",
+    :content_main => "You can also make `inline code` to add code into other things.",                                   :preview => "code",              :tags => "code",   :status => "Published", :category_id => second_category_id },
+  { :title => "Why is Basic Formatting Useful?",
+    :content_main => "Basic formatting of *italics* and **bold** is supported. This *can be **nested** like* so.",       :preview => "nested",            :tags => "nest",   :status => "Published", :category_id => second_category_id },
+  { :title => "Oh My Gosh, Headers. Why are they so Great?",
+    :content_main => "### Headings *can* also contain **formatting**",                                                   :preview => "header formatting", :tags => "format", :status => "Published", :category_id => second_category_id }
   ])
--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -2,12 +2,11 @@ require 'spec_helper'
 
 describe 'Categories' do
   describe 'user views a category page' do
-  	let(:category) { create(:category, name: 'parking', description: 'Foo') }
+    let(:category) { FactoryGirl.create(:category, name: 'parking', description: 'Foo') }
 
     it 'displays category details' do
       visit category_path(category)
       expect(page).to have_content('parking')
-      expect(page).to have_content('Foo')
     end
 
     it 'links to other categories in the sidebar using the slugged url' do
@@ -15,6 +14,27 @@ describe 'Categories' do
       visit category_path(category)
       expect(page).to have_content(other_category.name)
       expect(page.html.include?(other_category.slug)).to be true
+    end
+  end
+
+  describe 'user views the category index page' do
+    before do
+      category = FactoryGirl.create(:category, name: 'parking', description: 'Foo')
+      FactoryGirl.create(:article, title: "How do I park?", category: category)
+      FactoryGirl.create(:category, name: 'markets', description: 'where for food')
+      visit categories_path
+    end
+
+    it 'displays the category names and descriptions' do
+      expect(page).to have_content('parking')
+      expect(page).to have_content('Foo')
+      expect(page).to have_content('markets')
+      expect(page).to have_content('where for food')
+    end
+
+    it 'has links to the category details pages' do
+      click_link 'parking'
+      expect(page).to have_content("How do I park?")
     end
   end
 end
